bba7bb39a0df1714c79e90d2fb6362bdcce03f5b
Per Idar Rød <github@peridar.net>
2023-03-22 23:00:28 +0100

    Add files via upload

---
+++ users.py
@@ -0,0 +1,9 @@
+# add users here in this format.
+# add the newspapers and how many headlines each newspaper should have.
+# The program currently have 0-5 headlines as an option.
+user_dict = {
+    'Your Name': {'email': 'your-email@someserver.com',
+                  'newspapers': {'dagen': 2, 'h_avis': 2, 'jerusalem': 2,
+                                 'jærbladet': 3, 'os_fusa': 2, 'ringblad': 1,
+                                 'vg': 4, 'wsj': 4}}
+}
